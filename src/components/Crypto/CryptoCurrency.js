97a275ed3a03e0487393a62c6e2a416e6b98ae54
Egor Berezovsky <egor.berezovskiy.2017@mail.ru>
2024-04-26 12:57:02 +0300

    add input

--- src/components/Crypto/CryptoCurrency.js
+++ src/components/Crypto/CryptoCurrency.js
@@ -3,11 +3,15 @@ import "./CryptoCurrency.css";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from '@mui/material/MenuItem';
+
 
 export default function CrypoCyrrency() {
   return (
     <Container maxWidth="lg">
-      <div className="crypto-table">
+      {/* <div className="crypto-table">
         <h2>Текущие курсы Криптовалют</h2>
         <table>
           <thead>
@@ -19,13 +23,37 @@ export default function CrypoCyrrency() {
           </thead>
           <tbody></tbody>
         </table>
-      </div>
+      </div> */}
       <Grid className="crypto-table" container spacing={2}>
         <Grid xs={8}>
           <Paper className="crypto-currency-paper">xs=8</Paper>
         </Grid>
         <Grid xs={4}>
-          <Paper className="crypto-currency-paper">xs=4</Paper>
+          <Paper className="crypto-currency-input">
+            <div>
+            <TextField
+              error
+              id="outlined-error"
+              label="Error"
+              defaultValue="Hello World"
+            />
+                    <Select
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={10}
+          // onChange={handleChange}
+          autoWidth
+          label="Age"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Twenty</MenuItem>
+          <MenuItem value={21}>Twenty one</MenuItem>
+          <MenuItem value={22}>Twenty one and a half</MenuItem>
+        </Select>
+            </div>
+          </Paper>
         </Grid>
       </Grid>
     </Container>
