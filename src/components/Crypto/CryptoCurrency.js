ec85c02788ecbb27665b65205b68d04e7a67c5d9
Egor Berezovsky <egor.berezovskiy.2017@mail.ru>
2024-04-29 19:04:19 +0300

    add container

--- src/components/Crypto/CryptoCurrency.js
+++ src/components/Crypto/CryptoCurrency.js
@@ -1,57 +1,106 @@
-import React from "react";
-import "./CryptoCurrency.css";
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Unstable_Grid2";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import TextField from "@mui/material/TextField";
 import Select from "@mui/material/Select";
-import MenuItem from '@mui/material/MenuItem';
+import "./CryptoCurrency.css";
 
+const currencies = [
+  {
+    value: "USD",
+    label: "$",
+  },
+  {
+    value: "EUR",
+    label: "€",
+  },
+  {
+    value: "BTC",
+    label: "฿",
+  },
+  {
+    value: "JPY",
+    label: "¥",
+  },
+];
 
-export default function CrypoCyrrency() {
+export default function CryptoCurrency() {
   return (
-    <Container maxWidth="lg">
-      {/* <div className="crypto-table">
-        <h2>Текущие курсы Криптовалют</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Валюта</th>
-              <th>Курс НБРБ</th>
-              <th>Изменения</th>
-            </tr>
-          </thead>
-          <tbody></tbody>
-        </table>
-      </div> */}
-      <Grid className="crypto-table" container spacing={2}>
-        <Grid xs={8}>
-          <Paper className="crypto-currency-paper">xs=8</Paper>
+    <Container maxWidth="lg" className="crypto-container">
+      <Grid container spacing={3}>
+        <Grid xs={7}>
+          <Paper>xs=8</Paper>
         </Grid>
-        <Grid xs={4}>
-          <Paper className="crypto-currency-input">
-            <div>
-            <TextField
-              error
-              id="outlined-error"
-              label="Error"
-              defaultValue="Hello World"
-            />
-                    <Select
-          labelId="demo-simple-select-autowidth-label"
-          id="demo-simple-select-autowidth"
-          value={10}
-          // onChange={handleChange}
-          autoWidth
-          label="Age"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Twenty</MenuItem>
-          <MenuItem value={21}>Twenty one</MenuItem>
-          <MenuItem value={22}>Twenty one and a half</MenuItem>
-        </Select>
+        <Grid xs={5}>
+          <Paper>
+            <div className="select-conventor">
+              <FormControl sx={{ m: 1 }} variant="standard">
+                <TextField
+                  id="outlined-basic"
+                  label="Валюта"
+                  // variant="outlined"
+                />
+              </FormControl>
+              <FormControl sx={{ m: 1 }} variant="standard">
+                {/* <InputLabel
+                  id="demo-customized-select-label"
+                  helperText="Please select your currency"
+                ></InputLabel>
+                <TextField
+                  id="outlined-select-currency"
+                  select
+                  label="Select"
+                  defaultValue="EUR"
+                  helperText="Please select your currency"
+                >
+                  {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField> */}
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={10}
+                    label="Age"
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </FormControl>
+            </div>
+            <div className="select-conventor">
+              <FormControl sx={{ m: 1 }} variant="standard">
+                <TextField id="outlined-basic" label="Валюта" />
+              </FormControl>
+              <FormControl sx={{ m: 1 }} variant="standard">
+                <InputLabel
+                  id="demo-customized-select-label"
+                  helperText="Please select your currency"
+                ></InputLabel>
+                <TextField
+                  id="outlined-select-currency"
+                  select
+                  label="Select"
+                  defaultValue="EUR"
+                  helperText="Please select your currency"
+                >
+                  {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </FormControl>
             </div>
           </Paper>
         </Grid>
